17310556ba3f46b04023817f68b6c772e2fdd9d7
Takeshi Miyajima <rozyhead@gmail.com>
2017-08-23 01:30:33 +0900

    Add libraries

--- src/main/kotlin/jp/co/sunarch/apps/recordr/RecordrApplication.kt
+++ src/main/kotlin/jp/co/sunarch/apps/recordr/RecordrApplication.kt
@@ -1,10 +1,26 @@
 package jp.co.sunarch.apps.recordr
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.jetbrains.exposed.spring.SpringTransactionManager
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import javax.sql.DataSource
 
 @SpringBootApplication
-class RecordrApplication
+class RecordrApplication {
+
+  @Bean
+  fun kotlinModule() = KotlinModule()
+
+  @Bean
+  fun javaTimeModule() = JavaTimeModule()
+
+  @Bean
+  fun transactionManager(dataSource: DataSource) = SpringTransactionManager(dataSource)
+
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(RecordrApplication::class.java, *args)
